f3626e3e873864f9ee7652f8d512aca18994da84
mauriciosd <mauriciosd@hotmail.com>
2022-06-09 21:37:24 -0300

    Added loading workaround

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
@@ -21,8 +21,10 @@ import VideoPlayer from '../../components/UI/VideoPlayer/VideoPlayer'
 import ConvertSelection from '../../components/ConversionComponents/ConvertSelection/ConvertSelection'
 import PlaylistsPage from '../PlaylistsPage/PlaylistsPage'
 
+// BOTTOM WHITE FIX 
 
 function App() {
+  const [ loading, setLoading ] = useState(true);
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const { getMyProfile } = bindActionCreators(actionCreators, dispatch)
@@ -30,6 +32,7 @@ function App() {
 
   useEffect(() => {
     user.loggedIn && getMyProfile()
+    setTimeout(() => setLoading(false), 5000);
   }, [])
 
   return (
