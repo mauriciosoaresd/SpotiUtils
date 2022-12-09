8aef1e2e305cccc5072ee07e430e6aa045212ec6
mauricio <mauriciosoaresd@hotmail.com>
2022-12-09 18:10:08 -0300

    Testing env variables

--- src/components/HomeComponents/LoginButton/LoginButton.jsx
+++ src/components/HomeComponents/LoginButton/LoginButton.jsx
@@ -5,8 +5,6 @@ import styles from './LoginButton.module.css'
 const LoginButton = () => {
     const handleLogin = () => {
         window.location = `${process.env.REACT_APP_DOMAIN}/auth/spotify`
-        console.log("teste")
-
     }
     return(
         <button className={`${styles.login__button}`} type="submit" onClick={() => handleLogin()}>
