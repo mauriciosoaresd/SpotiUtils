449703bc4a0693ba90252a5b8d3bb48228f77708
mauricio <mauriciosoaresd@hotmail.com>
2022-12-09 18:20:33 -0300

    Testing env variables

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ app.use(passport.session())
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname,'build','favicon.ico')));
+// app.use(favicon(path.join(__dirname,'build','favicon.ico')));
+
+app.use(favicon(path.join('build','favicon.ico')));
 app.use(express.static(path.join(__dirname,'build')));
 
 // Routes
