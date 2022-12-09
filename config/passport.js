09f742bd95620f2c7a26b28dd836114ff5fe2076
mauricio <mauriciosoaresd@hotmail.com>
2022-12-09 16:41:51 -0300

    Testing env variables

--- config/passport.js
+++ config/passport.js
@@ -9,36 +9,35 @@ passport.use(new SpotifyStrategy({
     callbackURL: process.env.SPOTIFY_CALLBACK
 },
     function (accessToken, refreshToken, expires_in, profile, done) {
-        console.log("TESTETESTETESTE")
-        // User.findOne({ Spotify_User_Id: profile.id }, function (err, user) {
-        //     if (err) {
-        //         console.log('\x1b[31m', `Error trying to find user...\n${err}`)
-        //         done(err)
-        //     }
-        //     if (user) {
-        //         console.log('\x1b[36m%s\x1b[0m', 'User already on system, updating data...')
-        //         user.userName = profile.username
-        //         user.Spotify_User_Id = profile.id
-        //         user.Spotify_Access_Token = accessToken
-        //         user.Spotify_Refresh_Token = refreshToken
-        //         user.save()
-        //         return done(null, user)
-        //     } else {
-        //         console.log('\x1b[33m', 'New User, saving on database')
+        User.findOne({ Spotify_User_Id: profile.id }, function (err, user) {
+            if (err) {
+                console.log('\x1b[31m', `Error trying to find user...\n${err}`)
+                done(err)
+            }
+            if (user) {
+                console.log('\x1b[36m%s\x1b[0m', 'User already on system, updating data...')
+                user.userName = profile.username
+                user.Spotify_User_Id = profile.id
+                user.Spotify_Access_Token = accessToken
+                user.Spotify_Refresh_Token = refreshToken
+                user.save()
+                return done(null, user)
+            } else {
+                console.log('\x1b[33m', 'New User, saving on database')
 
-        //         var newUser = new User({
-        //             userName: profile.userName,
-        //             Spotify_User_Id: profile.id,
-        //             Spotify_Access_Token: accessToken,
-        //             Spotify_Refresh_Token: refreshToken,
-        //         })
+                var newUser = new User({
+                    userName: profile.userName,
+                    Spotify_User_Id: profile.id,
+                    Spotify_Access_Token: accessToken,
+                    Spotify_Refresh_Token: refreshToken,
+                })
 
-        //         newUser.save(function (err) {
-        //             if (err) return done(err)
-        //             return done(null, user)
-        //         })
-        //     }
-        // })
+                newUser.save(function (err) {
+                    if (err) return done(err)
+                    return done(null, user)
+                })
+            }
+        })
     }
 ))
 
