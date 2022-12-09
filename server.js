6d215ea701b0030b4345b8484f887893da638d77
mauricio <mauriciosoaresd@hotmail.com>
2022-12-09 18:22:04 -0300

    Testing env variables

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.use(express.json());
 
 // app.use(favicon(path.join(__dirname,'build','favicon.ico')));
 
-app.use(favicon(path.join('build','favicon.ico')));
 app.use(express.static(path.join(__dirname,'build')));
 
 // Routes
